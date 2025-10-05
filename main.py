94ae9fcb6701834d84a8704a67ac24d556066536
Jonas Daniel <ac3ofsp4des893@gmail.com>
2025-10-05 21:12:31 +0200

    added count_characters function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 
 def get_book_text(path):
     with open(path) as b:
@@ -9,6 +10,7 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     print (f"Found {num_words} total words")
+    print (count_characters(text))
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,2 +1,19 @@
+# Counts the words in a text
+
 def count_words(text):
     return len(text.split())
+
+# Counts single characters in a text
+
+def count_characters(text):
+    charcters = {}
+    for c in text.lower():
+        if c in charcters:
+            charcters[c] += 1
+        else:
+            charcters[c] = 1
+    return charcters
+
+    
+
+
