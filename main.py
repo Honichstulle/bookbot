e7822bebd04ee86c89f1995dc732a8df1ea6e879
Jonas Daniel <ac3ofsp4des893@gmail.com>
2025-10-07 08:24:37 +0200

    added command line input

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ def get_book_text(path):
         return b.read()
     
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = count_words(text)
-    num_characters = count_characters(text)
-    print ("============ BOOKBOT ============")
-    print (f"Analyzing book found at {book_path}...")
-    print ("----------- Word Count ----------")
-    print (f"Found {num_words} total words")
-    print ("----------- Character Count ----------")
-    for c in sort_dictionaries(num_characters):
-        if c["char"].isalpha() == False:
-            continue
-        else:
-            char = c["char"]
-            num = c["num"]
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 main.py <path_to_book>")
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = count_words(text)
+        num_characters = count_characters(text)
+        print ("============ BOOKBOT ============")
+        print (f"Analyzing book found at {book_path}...")
+        print ("----------- Word Count ----------")
+        print (f"Found {num_words} total words")
+        print ("----------- Character Count ----------")
+        for c in sort_dictionaries(num_characters):
+            if c["char"].isalpha() == False:
+                continue
+            else:
+                char = c["char"]
+                num = c["num"]
             print(f"{char}: {num}")
-    print("============= END ===============")
+        print("============= END ===============")
 
 
 main()
